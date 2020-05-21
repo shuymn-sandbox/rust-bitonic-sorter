940e2aec29833cd53bf5905f85cd2deccf2ac536
Shu Yamani <mail@shuymn.me>
2020-05-21 18:59:23 +0900

    use iterator chain

--- src/utils.rs
+++ src/utils.rs
@@ -4,21 +4,10 @@ use rand_pcg::Pcg64Mcg;
 
 pub fn new_u32_vec(n: usize) -> Vec<u32> {
     // RNGを初期化する。再現性を持たせるため毎回同じシード値を使う
-    let mut rng = Pcg64Mcg::from_seed([0; 16]);
+    let rng = Pcg64Mcg::from_seed([0; 16]);
 
-    // n個の要素が格納できるようベクタを初期化する
-    let mut v = Vec::with_capacity(n);
-
-    // 0からn-1までの合計n回、繰り返し乱数を生成し、ベクタに追加する
-    // (0からn-1の数列は使わないので、_で受けることですぐに破棄している)
-    for _ in 0..n {
-        // RNGのsampleメソッドは引数として与えられた分布に従う乱数を1つ生成する
-
-        // Standard分布は生成する値が数値型(ここではu32型)のときは一様分布になる
-        // つまり、その型が取りうる全ての値が同じ確率で出現する
-        v.push(rng.sample(&Standard));
-    }
-
-    // ベクタを返す
-    v
+    // rng.sample_iter()は乱数を無限に生成するイテレータを返す
+    // take(n)は元のイテレータから最初のn要素だけを取り出すイテレータを返す
+    // collect()はイテレータから値を収集して、ベクタやハッシュマップのようなコレクションに格納する
+    rng.sample_iter(&Standard).take(n).collect()
 }
